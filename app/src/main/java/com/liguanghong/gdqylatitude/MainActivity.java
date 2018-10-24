d9385c4cec162bbd4fea72b434716b9010314d9c
1947501227 <1947501227@qq.com>
2018-10-24 11:40:50 +0800

    1

--- app/src/main/java/com/liguanghong/gdqylatitude/MainActivity.java
+++ app/src/main/java/com/liguanghong/gdqylatitude/MainActivity.java
@@ -2,13 +2,37 @@ package com.liguanghong.gdqylatitude;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.ZoomControls;
+
+import com.baidu.mapapi.SDKInitializer;
+import com.baidu.mapapi.map.BaiduMap;
+import com.baidu.mapapi.map.MapView;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        SDKInitializer.initialize(getApplicationContext());
+        setContentView(R.layout.fragment_map);
+
+        MapView mMapView;
+        BaiduMap mBaiduMap;
+        mMapView = (MapView) findViewById(R.id.map);
+        mBaiduMap = mMapView.getMap();
+
+        //隐藏百度地图logo
+        View child = mMapView.getChildAt(1);
+        if (child != null && (child instanceof ImageView || child instanceof ZoomControls)){
+            child.setVisibility(View.INVISIBLE);
+        }
+        //地图上比例尺
+        mMapView.showScaleControl(true);
+        // 隐藏缩放控件
+        mMapView.showZoomControls(true);
+
     }
 }
