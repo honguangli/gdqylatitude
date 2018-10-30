446a8b731318fa013123331a170b7750a4a5d316
LGH <1947501227@qq.com>
2018-10-30 23:01:22 +0800

    li

---
+++ app/src/main/java/com/liguanghong/gdqylatitude/activitys/ResetPwdActivity.java
@@ -0,0 +1,18 @@
+package com.liguanghong.gdqylatitude.activitys;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+import com.liguanghong.gdqylatitude.R;
+
+public class ResetPwdActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_resetpassword);
+
+    }
+
+}
---
+++ app/src/main/java/com/liguanghong/gdqylatitude/activitys/SignUpActivity.java
@@ -0,0 +1,17 @@
+package com.liguanghong.gdqylatitude.activitys;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.liguanghong.gdqylatitude.R;
+
+public class SignUpActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup);
+
+    }
+
+}
