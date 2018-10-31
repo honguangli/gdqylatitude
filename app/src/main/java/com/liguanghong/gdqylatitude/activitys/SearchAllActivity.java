16c396ba3ace7589791b1fad013536fd304a2179
LGH <1947501227@qq.com>
2018-10-31 22:36:20 +0800

    liguanghong

---
+++ app/src/main/java/com/liguanghong/gdqylatitude/activitys/SearchAllActivity.java
@@ -0,0 +1,12 @@
+package com.liguanghong.gdqylatitude.activitys;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SearchAllActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
