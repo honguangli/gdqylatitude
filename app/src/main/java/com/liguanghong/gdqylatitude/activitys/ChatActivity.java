d899435be02566789ea0ac07f27219cb6b65993a
镜子 <1157517795@qq.com>
2018-11-08 21:12:33 +0800

    cheng

--- app/src/main/java/com/liguanghong/gdqylatitude/activitys/ChatActivity.java
+++ app/src/main/java/com/liguanghong/gdqylatitude/activitys/ChatActivity.java
@@ -1,11 +1,17 @@
 package com.liguanghong.gdqylatitude.activitys;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
 import android.view.Gravity;
+import android.view.MotionEvent;
 import android.view.View;
+import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.TranslateAnimation;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.BaseAdapter;
 import android.widget.EditText;
 import android.widget.ImageView;
@@ -27,20 +33,33 @@ import com.liguanghong.gdqylatitude.manager.UserManager;
 import java.io.UnsupportedEncodingException;
 import java.util.List;
 
-public class ChatActivity extends BaseActivity implements View.OnClickListener {
+import cn.dreamtobe.kpswitch.util.KPSwitchConflictUtil;
 
-    private ImageView backtrack_friend_chat;                //返回通讯录
-    private TextView tv_friendName;                          //好友名称
-    private TextView tv_friendState;                         //好友状态
-    private TextView tv_data;                                 //好友详情
-    private TextView tv_send;                                 //发送消息
-    private RelativeLayout rly_add;                           //添加图片，位置
-    private EditText ed_content;                              //要发送的文字内容
+public class ChatActivity extends BaseActivity implements View.OnClickListener, View.OnTouchListener {
+
+    private ImageView backtrack_friend_chat;                    //返回通讯录
+    private TextView tv_friendName;                             //好友名称
+    private TextView tv_friendState;                            //好友状态
+    private TextView tv_data;                                   //好友详情
+    private TextView tv_send;                                   //发送消息
+    private TextView hint_tv_pic;                               //调用相册
+    private TextView hint_tv_phone;                             //调用相机
+    private TextView hint_tv_location;                          //获取定位信息
+    private RelativeLayout add;                                 //添加图片，位置
+    private RelativeLayout contentPanel_bottomPanel;          //信息发送栏
+    private RelativeLayout contentPanel_bottomPanel_hint;     //底部弹出栏
+    private EditText ed_content;                                //要发送的文字内容
     private ListView message_listView;
     private ChatAdapter chatAdapter;
     private static Handler chatHandler;
 
     private User friend;                                   //好友ID
+
+    private boolean isExpanded = true;                  //判断底部弹出栏的状态
+    private ViewGroup.LayoutParams pp;                     //用于改变信息发送栏的位置
+
+    private InputMethodManager inputMethodManager;      //用于隐藏软键盘
+    private TranslateAnimation mShowAction;             //补间动画，用于底部弹出栏
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -59,14 +78,37 @@ public class ChatActivity extends BaseActivity implements View.OnClickListener {
         tv_friendState = (TextView) findViewById(R.id.tv_friendState);
         tv_data = (TextView) findViewById(R.id.tv_data);
         tv_send = (TextView) findViewById(R.id.tv_send);
-        rly_add = (RelativeLayout)findViewById(R.id.add);
+        hint_tv_pic = findViewById(R.id.hint_tv_pic);
+        hint_tv_phone = findViewById(R.id.hint_tv_phone);
+        hint_tv_location = findViewById(R.id.hint_tv_location);
+        add = (RelativeLayout)findViewById(R.id.add);
+        contentPanel_bottomPanel = findViewById(R.id.contentPanel_bottomPanel);
+        contentPanel_bottomPanel_hint = findViewById(R.id.contentPanel_bottomPanel_hint);
         ed_content = (EditText) findViewById(R.id.ed_content);
         message_listView = (ListView) findViewById(R.id.message_listView);
+
+        pp = contentPanel_bottomPanel.getLayoutParams();
+
+        inputMethodManager = (InputMethodManager)this.getSystemService(Context.INPUT_METHOD_SERVICE);
+
+        mShowAction = new TranslateAnimation(Animation.RELATIVE_TO_SELF, 0.0f,
+                Animation.RELATIVE_TO_SELF, 0.0f, Animation.RELATIVE_TO_SELF,
+                1.0f, Animation.RELATIVE_TO_SELF, 0.0f);
+        mShowAction.setDuration(300);
+
+        ed_content.setFocusable(true);
+        ed_content.setFocusableInTouchMode(true);
+        ed_content.requestFocus();
 
         backtrack_friend_chat.setOnClickListener(this);
         tv_data.setOnClickListener(this);
         tv_send.setOnClickListener(this);
-        rly_add.setOnClickListener(this);
+        add.setOnClickListener(this);
+        ed_content.setOnClickListener(this);
+        hint_tv_pic.setOnClickListener(this);
+        hint_tv_phone.setOnClickListener(this);
+        hint_tv_location.setOnClickListener(this);
+        message_listView.setOnTouchListener(this);
 
     }
 
@@ -118,13 +160,63 @@ public class ChatActivity extends BaseActivity implements View.OnClickListener {
                 break;
 
             case R.id.add:                          //添加图片，地理位置
+                inputMethodManager.hideSoftInputFromWindow(this.getCurrentFocus().getWindowToken(), InputMethodManager.HIDE_NOT_ALWAYS);//隐藏软键盘
+
+                if (isExpanded) {
+                    openRoot();
+                } else {
+                    closeRoot();
+                }
+                break;
+
+            case R.id.ed_content:                   //编辑框
+                closeRoot();
+                break;
+
+            case R.id.hint_tv_pic:                  //调用相册
+
+                break;
+
+            case R.id.hint_tv_phone:                //调用相机
+
+                break;
+
+            case R.id.hint_tv_location:             //获取位置信息
 
                 break;
 
             case R.id.backtrack:            //返回通讯录
+                inputMethodManager.hideSoftInputFromWindow(this.getCurrentFocus().getWindowToken(), InputMethodManager.HIDE_NOT_ALWAYS);//隐藏软键盘
+                closeRoot();
                 finish();
                 break;
         }
     }
+    //打开底部弹出框
+    private void openRoot(){
+        pp.height = 800;
+        contentPanel_bottomPanel.startAnimation(mShowAction);
+        contentPanel_bottomPanel.setLayoutParams(pp);
+        contentPanel_bottomPanel_hint.startAnimation(mShowAction);
+        contentPanel_bottomPanel_hint.setVisibility(View.VISIBLE);
+        isExpanded = false;
+    }
 
+    //关闭底部弹出框
+    private void closeRoot(){
+        pp.height = 120;
+        contentPanel_bottomPanel.setLayoutParams(pp);
+        contentPanel_bottomPanel_hint.setVisibility(View.INVISIBLE);
+        isExpanded = true;
+    }
+
+    @Override
+    //点击屏幕软键盘隐藏，底部栏隐藏
+    public boolean onTouch(View view, MotionEvent motionEvent) {
+        if (motionEvent.getAction() == MotionEvent.ACTION_DOWN) {
+            closeRoot();
+            KPSwitchConflictUtil.hidePanelAndKeyboard(contentPanel_bottomPanel_hint);
+        }
+        return false;
+    }
 }
